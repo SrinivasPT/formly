e8bf96bbd080bca7e8fe559e196a8afe5e05f42e
Srini <srinivaspt@outlook.com>
2019-07-20 12:43:15 +0530

    before upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LabelModule } from '@progress/kendo-angular-label';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { DialogsModule } from '@progress/kendo-angular-dialog';
+import { EditorModule } from '@progress/kendo-angular-editor';
+import { ExcelExportModule } from '@progress/kendo-angular-excel-export';
+
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +24,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonsModule,
+    BrowserAnimationsModule,
+    LabelModule,
+    DateInputsModule,
+    DialogsModule,
+    EditorModule,
+    ExcelExportModule
   ],
   providers: [],
   bootstrap: [AppComponent]
